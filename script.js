cd0f3d086990298dbafcf08b807680545794fa73
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-05-10 23:21:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+
+function daysOfYear(year) {
+	if (year % 4 === 0) {
+		if (year % 100 === 0) {
+			if (year % 400 === 0) {
+				return 366;
+			}
+			else{
+				return 365;
+			}
+		}else{
+			return 366;
+		}
+	
+	}else{
+		return 365;
+	}
+}
